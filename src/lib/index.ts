759f287cc311601b1d7e9112586f0d4cc2b42891
Thibault <tbmc33@gmail.com>
2024-10-09 14:16:25 +0200

    Add code which throws errors on Galaxy Tab S8

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,76 @@
-// place files you want to import through the `$lib` alias in this folder.
+import Horses from './horses.jpg';
+
+export async function downloadImage(): Promise<Uint8Array> {
+	return new Promise((resolve) => {
+		const img = new Image();
+		img.src = Horses;
+		img.onload = () => {
+			const canvas = new OffscreenCanvas(img.width, img.height);
+			const context = canvas.getContext('2d')!;
+			context.drawImage(img, 0, 0);
+			const imageData = context.getImageData(0, 0, 640, 640); // I don't take full image, but I don't care
+			resolve(new Uint8Array(imageData?.data.buffer));
+		};
+	});
+}
+
+export function prepareToDetectYoloV7(image: Uint8Array | Float32Array): Float32Array {
+	const resultTotalSize = 640 * 640 * 3;
+	const output = new Float32Array(resultTotalSize);
+
+	const rowLength = 640 * 4;
+	if (image.length !== rowLength * 640) {
+		throw new Error();
+	}
+
+	const tempShape = [640, 640, 3];
+	const newShape = [2, 0, 1].map((i) => tempShape[i]);
+	const rRowLength = newShape[1] * newShape[2];
+
+	for (let row = 0; row < 640; row++) {
+		const rowStart = row * rowLength;
+
+		for (let col = 0; col < 640; col++) {
+			const colStart = col * 4;
+
+			for (let color = 0; color < 3; color++) {
+				const value = image[rowStart + colStart + color];
+				output[color * rRowLength + row * newShape[2] + col] = value / 255;
+			}
+		}
+	}
+
+	return output;
+}
+
+export interface ConsoleErrorOverride {
+	originalConsoleError(...data: unknown[]): void;
+	restoreConsoleError(): void;
+	errorCalled(): boolean;
+}
+
+export function overrideConsoleError(
+	label: string,
+	callback?: (...data: unknown[]) => void
+): ConsoleErrorOverride {
+	const originalConsoleError = console.error;
+	let errorHasBeenCalled = false;
+
+	console.error = (...data: unknown[]) => {
+		errorHasBeenCalled = true;
+		console.group(`Captured error : ${label}`);
+		originalConsoleError(...data);
+		if (callback) callback(...data);
+		console.groupEnd();
+	};
+
+	return {
+		originalConsoleError,
+		restoreConsoleError() {
+			console.error = originalConsoleError;
+		},
+		errorCalled() {
+			return errorHasBeenCalled;
+		}
+	};
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,45 @@
 <script lang="ts">
+	import { downloadImage, overrideConsoleError, prepareToDetectYoloV7 } from '$lib';
 	import * as ort from 'onnxruntime-web/webgpu';
 
-	ort.env.wasm.numThreads = 4;
+	ort.env.wasm.numThreads = 0;
 	ort.env.wasm.wasmPaths = '/js/';
 
 	let isWorking: boolean | undefined = undefined;
+	let result: string = '';
+	let errorList: string[] = [];
 
 	async function init() {
+		errorList = [];
+		const consoleOverride = overrideConsoleError('ONNX Runtime', (...data) =>
+			errorList.push(JSON.stringify(data))
+		);
 		try {
+			const img = await downloadImage();
+			const prepared = prepareToDetectYoloV7(img);
+
 			const yolo = await ort.InferenceSession.create('/yolov7-tiny.onnx', {
-				executionProviders: ['webgpu']
+				executionProviders: ['webgpu'],
+				logSeverityLevel: 3
 			});
 
-			const array = new Float32Array(640 * 640 * 3);
-			const tensor = new ort.Tensor('float32', array, [1, 3, 640, 640]);
+			const tensor = new ort.Tensor('float32', prepared, [1, 3, 640, 640]);
 
 			const inputName = yolo.inputNames[0];
 			const outputName = yolo.outputNames[0];
 
 			const inferenceResult = await yolo.run({ [inputName]: tensor });
 			const output = inferenceResult[outputName];
+
+			result = JSON.stringify(output);
 			console.log('Output: ', output);
 
-			isWorking = true;
-		} finally {
-			console.error('There was an error!');
+			isWorking = !consoleOverride.errorCalled();
+		} catch (e) {
+			console.error('There was an error!', e);
 			isWorking = false;
+		} finally {
+			consoleOverride.restoreConsoleError();
 		}
 	}
 
@@ -36,4 +50,30 @@
 	<div>Loading</div>
 {:else}
 	<div>Is it working: {isWorking}</div>
+
+	{#if isWorking}
+		<p>ONNX Runtime has used the model correctly with GPU.</p>
+		<div class="result">Result: {result}</div>
+	{:else}
+		<p>See Dev Tools Console for more information</p>
+		<p>Errors :</p>
+		<div>
+			{#each errorList as error}
+				<p class="error-container">
+					{error}
+				</p>
+			{/each}
+		</div>
+	{/if}
 {/if}
+
+<style>
+	.result {
+		text-wrap: wrap;
+		line-break: anywhere;
+	}
+
+	.error-container {
+		border-bottom: 1px solid black;
+	}
+</style>
